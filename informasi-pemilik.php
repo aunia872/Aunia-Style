1a78e0712a95a7ccd19cde38540fac12d3d92a88
aunia872 <tazkiyaturrahmahaunia@gmail.com>
2024-12-29 13:06:46 +0700

    Add files via upload

---
+++ informasi-pemilik.php
@@ -0,0 +1,10 @@
+<?php
+// informasi-pemilik.php
+$data = [
+    'foto' => 'nia.jpg',
+    'nama' => 'Aunia Tazkiyaturrahmah',
+    'deskripsi' => 'Nama saya Aunia Tazkiyaturrahmah. Sejak kecil, saya memiliki ketertarikan dalam dunia fashion dan gaya hidup. Saya memulai Sweet Style dengan tujuan untuk menyediakan pakaian yang tidak hanya modis, tetapi juga terjangkau untuk semua orang. Saya berkomitmen untuk selalu memberikan produk berkualitas dan layanan pelanggan yang luar biasa.'
+];
+
+echo json_encode($data);
+?>
